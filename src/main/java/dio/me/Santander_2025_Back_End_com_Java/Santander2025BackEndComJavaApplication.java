8324b2796b2400337f50edc45b5bb80c966b1d59
Richard Merces <richardmerces@outlook.com>
2025-07-26 13:14:46 -0300

    Commit inicial

---
+++ src/main/java/dio/me/Santander_2025_Back_End_com_Java/Santander2025BackEndComJavaApplication.java
@@ -0,0 +1,13 @@
+package dio.me.Santander_2025_Back_End_com_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santander2025BackEndComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santander2025BackEndComJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/Santander_2025_Back_End_com_Java/Santander2025BackEndComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.Santander_2025_Back_End_com_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Santander2025BackEndComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
